58a747c25f5248666c78319c91518c3636498aad
kishanhitk <kishansharma1231@gmail.com>
2021-05-21 20:46:54 +0530

    ✨ Changed logic for selecting accepted answer

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const scrollToAnswer = () =>
-  document
-    .getElementsByClassName("iconCheckmarkLg")[0]
-    .parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.scrollIntoView(
-      { behavior: "smooth" }
-    );
+const scrollToAnswer = () => {
+  const htmlCollection = document.getElementsByClassName(
+    "js-accepted-answer-indicator"
+  );
+
+  var n = htmlCollection.length;
+  
+  for (var i = 0; i < n; i++) {
+    if (htmlCollection[i].className.search("d-none") === -1) {
+      htmlCollection[i].parentElement.parentElement.scrollIntoView({
+        behavior: "smooth",
+      });
+      break;
+    }
+  }
+};
+
 window.onload = () => scrollToAnswer();
